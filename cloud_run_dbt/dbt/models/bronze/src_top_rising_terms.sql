442b2af6eb1557cf050ffe4a3e34b41a2fa72c7c
david-petrovic <david.petrovic@akane.waseda.jp>
2026-06-06 09:45:12 +0200

    feat: define BigQuery Google Trends source layer

--- cloud_run_dbt/dbt/models/bronze/src_top_rising_terms.sql
+++ cloud_run_dbt/dbt/models/bronze/src_top_rising_terms.sql
@@ -1,8 +1,16 @@
-with source as (
+with latest_refresh_date as (
+
+    select max(refresh_date) as refresh_date
+    from {{ source('google_trends', 'top_rising_terms') }}
+
+),
+
+source as (
+
     select *
-    from `bigquery-public-data.google_trends.top_rising_terms`
-    where refresh_date = "2026-05-13"
-    limit 100 -- limit does not impact big-query costs!
+    from {{ source('google_trends', 'top_rising_terms') }}
+    where refresh_date = (select refresh_date from latest_refresh_date)
+
 )
 
 select * from source
